83d5774638870331f2f31667f8dca5e56d7084d5
anovis <anovis@ymail.com>
2021-03-31 09:52:29 -0400

    fix new line

--- tools/c7n_gcp/c7n_gcp/query.py
+++ tools/c7n_gcp/c7n_gcp/query.py
@@ -286,6 +286,7 @@ class ChildResourceManager(QueryResourceManager):
     @staticmethod
     def _extract_fields(source, mappings):
         result = {}
+
         for mapping in mappings:
             result[mapping[1]] = jmespath.search(mapping[0], source)
 
